9557bc1aac78729ae939536b6d50053089396c9c
geffrys <geffry.ospina@gmail.com>
2023-11-19 23:32:06 -0500

    HU5: fixing error introduced in previous commit

--- Backend/routes/session.routes.js
+++ Backend/routes/session.routes.js
@@ -1,10 +1,9 @@
-import {Router} from 'express'
-import { getSessions, postSessions } from '../controllers/session.controllers'
+import { Router } from 'express'
+import { getSessions, postSessions } from '../controllers/session.controllers.js'
 
 const router = Router()
 
 router.get('/', getSessions)
 router.post('/', postSessions)
-
 
 export default router
